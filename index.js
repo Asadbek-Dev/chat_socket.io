686b0222c933896429598aa8e46157df24a0a6e0
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-01-23 22:16:17 +0500

    connected

--- index.js
+++ index.js
@@ -10,4 +10,17 @@ server.listen(3000);
 
 app.get('/', function (request, respons) {
     respons.sendFile(__dirname + '/index.html')
+});
+
+users=[];
+connections=[];
+
+io.sockets.on('connection',function(socket){
+    console.log("Omadli ulanish!!!");
+    connections.push(socket);
+
+    socket.on('disconnect',function(data){
+        connections.splice(connections.indexOf(socket),1);
+        console.log("O'chirildi");
+    })
 })
